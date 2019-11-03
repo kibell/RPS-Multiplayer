bb3b4f0d9399ae2dc1d7dbaaeaf4fff981e9f426
Kitrick Bell <kitrickd.bell@gmail.com>
2019-11-03 13:37:47 -0600

    adding more images to the html

--- assets/game.js
+++ assets/game.js
@@ -1,8 +1,56 @@
 let p1Score = 0;
 let p2score = 0;
 
+const game ={
+numberOfConnections:0,
+playerNum: 0,
+
+player1:{
+    isConnected: false
+},
+
+player2:{
+
+isConnected: false
+
+}
+
+}
   
 let database = firebase.database()
 console.log(database)
+
+//variable needed for two players
+
+//Store scores when the a player wins
+
+
+
+//variable for the choice the player made
+
+//variable for the turn
+
+
+
+//listener to the database /players/ node to listen for any changes
+
+
+
+//check for player 1
+
+
+//check for player 2
+
+
+
+//when both players are present start game
+
+
+
+
+//game logic that recognizes when it is player 1 turn
+
+//rps logic that compares player 1 choice and player 2 choice and if player wins increase score
+
 
 
